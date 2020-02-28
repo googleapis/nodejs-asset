521ccb5ebecbd6773bf053292313516a918069ad
summerji <summerji@google.com>
2020-02-27 16:05:18 -0800

    chore: update jsdoc - protos and double quote

--- .jsdoc.js
+++ .jsdoc.js
@@ -42,7 +42,7 @@ module.exports = {
     systemName: '@google-cloud/asset',
     theme: 'lumen',
     default: {
-      "outputSourceFiles": false
+      outputSourceFiles: false
     }
   },
   markdown: {
